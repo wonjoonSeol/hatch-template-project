6f1a65609667f8a2754f7221509fc181702b59c5
Wonjoon Seol <wonjoon.seol@kcl.ac.uk>
2017-11-25 18:52:39 +0000

    added buttons

--- docs/javascript.js
+++ docs/javascript.js
@@ -128,3 +128,10 @@ function explodePie (e) {
 	e.chart.render();
 
 }
+
+// query button
+$('#myButton').on('click', function () {
+    var $btn = $(this).button('loading')
+    // business logic...
+    $btn.button('reset')
+})
